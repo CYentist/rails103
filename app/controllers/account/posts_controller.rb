80c6283f71f82eaaa8c5484293d09c7b0ad6fdd7
seewhymoon <seewhymoon@gmail.com>
2017-01-14 16:51:25 +0800

    add edit and delete in myposts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,8 @@
 class Account::PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @posts = current_user.posts
   end
+
 end
